3f6e05e9da68d25cdf6a4af8669ee619cee5fc54
Vallabh Kansagara <vrkansagara@gmail.com>
2020-10-28 07:52:18 +0530

    Freebsd post added

---
+++ data/blog/2020/2020-11-28-hello-freebsd.php
@@ -0,0 +1,47 @@
+<?php
+
+use PhlyBlog\AuthorEntity;
+use PhlyBlog\EntryEntity;
+
+$entry  = new EntryEntity();
+$author = new AuthorEntity();
+$author->fromArray([
+    'id'    => 'vrkansagara',
+    'name'  => 'Vallabh Kansagara',
+    'email' => 'vrkansagara@gmail.com',
+    'url'   => 'https://vrkansagara.in',
+]);
+
+$entry->setId(pathinfo(__FILE__, PATHINFO_FILENAME));
+$entry->setTitle('Hello FreeBSD');
+$entry->setAuthor($author);
+$entry->setDraft(false);
+$entry->setPublic(true);
+$entry->setCreated(new DateTime('2020:11:28 07:46:00'));
+$entry->setUpdated(new DateTime('2020:11:28 07:46:00'));
+$entry->setTimezone('Asia/Kolkata');
+$entry->setTags(['freebsd', 'shell']);
+
+$body = <<<'EOT'
+Hello FreeBSD
+
+![Hello FreeBSD](/assets/images/blog/FreeBSD/logo.png)
+
+I am starting this series using Facebook page at[AskFreeBSD](https://www.facebook.com/AskFreeBSD). 
+
+EOT;
+$entry->setBody(convertMarkdownToHtml($body));
+
+$extended = <<<'EOT'
+
+This is not official page by FreeBSD, But I am trying to provide best knowledge of what `FreeBSD` is how it help to you and your goal.
+
+![FreeBSD](/assets/images/blog/FreeBSD/logo-red.png)
+
+
+EOT;
+
+
+$entry->setExtended(convertMarkdownToHtml($extended));
+
+return $entry;
