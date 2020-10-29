de2bd0b475437ee39ae217bddc7ebf682f1b95b4
Vallabh Kansagara <vrkansagara@gmail.com>
2020-10-29 07:23:43 +0530

    Adding git command into blog post.
    Signed-off-by: Vallabh Kansagara <vrkansagara@gmail.com>

--- data/blog/2019/2019-09-10-git-useful-commands.php
+++ data/blog/2019/2019-09-10-git-useful-commands.php
@@ -13,12 +13,12 @@ $author->fromArray([
 ]);
 
 $entry->setId(pathinfo(__FILE__, PATHINFO_FILENAME));
-$entry->setTitle('git useful commands');
+$entry->setTitle('Git useful commands');
 $entry->setAuthor($author);
 $entry->setDraft(false);
 $entry->setPublic(true);
 $entry->setCreated(new DateTime('2019:09:10 16:33:48'));
-$entry->setUpdated(new DateTime('2019:09:10 16:35:03'));
+$entry->setUpdated(new DateTime('2020:11:29 07:18:00'));
 $entry->setTimezone('Asia/Kolkata');
 $entry->setTags(['linux','git']);
 
@@ -26,44 +26,56 @@ $body = <<<'EOT'
 * Store all your cred into system helper 
 
 ~~~bash
-    git config --global credential.helper store
-    # Set the cache to timeout after 1 hour (setting is in seconds)
-    git config --global credential.helper 'cache --timeout=3600'
+git config --global credential.helper store
+# Set the cache to timeout after 1 hour (setting is in seconds)
+git config --global credential.helper 'cache --timeout=3600'
 ~~~
 EOT;
 $entry->setBody(convertMarkdownToHtml($body));
 $extended = <<<'EOT'
+* Ignore file mode change in current working tree
+
+~~~bash
+# For individual project
+git config core.fileMode false 
+# For system wide all git project
+git config --global core.fileMode false
+~~~
 
 * Reset current branch to previous stat
 
 ~~~bash
-    git reset --hard HEAD
+git reset --hard HEAD
 ~~~
 
-
-* Ignore file mode change in current working tree
+* How to save your work into git clipboard
 
 ~~~bash
-    # For individual project
-    git config core.fileMode false 
-    # For system wide all git project
-    git config --global core.fileMode false
+git stash
 ~~~
 
+* Easy way to update your current code using same branch.
 
+~~~bash
+git pull --rebase
+~~~
 
 #### Final my `.gitconfig` file looks like
 
 ~~~bash
-~/.gitconfig 
-    [user]
-    	email = vrkansagara@gmail.com
-    	name = Vallabh Kansagara
-    [credential]
-    	helper = store
-    [core]
-    	fileMode = false
+cat ~/.gitconfig
 ~~~
+
+~~~bash 
+[user]
+    email = vrkansagara@gmail.com
+    name = Vallabh Kansagara
+[credential]
+    helper = store
+[core]
+    fileMode = false
+~~~
+
 EOT;
 
 
