72a095364b9fb999c2c48d7920e7efbe1a5c3f7c
Vallabh Kansagara <vrkansagara@gmail.com>
2020-11-02 10:04:26 +0530

    Removal of "text-decoration-none" class
    Signed-off-by: Vallabh Kansagara <vrkansagara@gmail.com>

--- resources/js/components/HomeComponent.vue
+++ resources/js/components/HomeComponent.vue
@@ -12,8 +12,10 @@
         <ul>
           <li>A day by Software Engineering.</li>
           <li>An Open Source Contributor.</li>
-          <li>Also I do tweet known as <a href="https://twitter.com/vrkansagara">@vrkansagara</a></li>
-          <li>Open source contributor at <a href="https://github.com/vrkansagara">@GitHub</a> with various projects.
+          <li>Also I do tweet known as <a class="text-decoration-none" href="https://twitter.com/vrkansagara">@vrkansagara</a>
+          </li>
+          <li>Open source contributor at <a class="text-decoration-none"
+                                            href="https://github.com/vrkansagara">@GitHub</a> with various projects.
           </li>
         </ul>
       </div>
